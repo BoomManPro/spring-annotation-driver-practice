0379292dbd7459fc660c00355a52ca31e16b70b0
wangqimeng <wangqimeng@mininglamp.com>
2019-07-12 16:47:11 +0800

    add BeanFactory Example

---
+++ src/main/java/cn/boommanpro/componentregister7/ComponentRegister7Test.java
@@ -0,0 +1,10 @@
+package cn.boommanpro.componentregister7;
+
+/**
+ * BeanFactory 使用示例
+ * @author <a href="mailto:boommanpro@gmail.com">BoomManPro</a>
+ * @date 2019/7/12 16:43
+ * @created by BoomManPro
+ */
+public class ComponentRegister7Test {
+}
