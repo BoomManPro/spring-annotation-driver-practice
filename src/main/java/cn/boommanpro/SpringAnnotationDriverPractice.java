075e07f30c09bd21db0a774846137157ddc294e4
wangqimeng <wangqimeng@mininglamp.com>
2019-07-15 17:05:47 +0800

    update description

--- src/main/java/cn/boommanpro/SpringAnnotationDriverPractice.java
+++ src/main/java/cn/boommanpro/SpringAnnotationDriverPractice.java
@@ -3,6 +3,15 @@ package cn.boommanpro;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
+ *
+ * annotation @Configuration example {@link cn.boommanpro.componentregister1.ComponentRegister1Test}
+ * annotation @Component and @ComponentScan example {@link cn.boommanpro.componentregister2.ComponentRegister2Test}
+ * annotation @Conditional example {@link cn.boommanpro.componentregister3.ComponentRegister3Test}
+ * annotation @Import example {@link cn.boommanpro.componentregister4.ComponentRegister4Test}
+ * interface ImportSelector example {@link cn.boommanpro.componentregister5.ComponentRegister5Test}
+ * interface ImportBeanDefinitionRegistrar example {@link cn.boommanpro.componentregister6.ComponentRegister6Test}
+ * interface FactoryBean example {@link cn.boommanpro.componentregister7.ComponentRegister7Test}
+ *
  * @author <a href="mailto:boommanpro@mininglamp.com">BoomManPro</a>
  * @date 2019/7/10 14:54
  * @created by BoomManPro
